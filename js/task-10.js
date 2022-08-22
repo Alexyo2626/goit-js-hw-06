ea14c5f8e370b5797affefa2194a191fd35080d3
Aleksey Knigin <alexyo2626@gmail.com>
2022-08-22 15:10:27 +0300

    add task_10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', createBoxes);
+
+// создание
+function createBoxes(amount) {
+  amount = inputEl.value;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const divBoxChildEl = document.createElement('div');
+    divBoxChildEl.style.width = `${30 + i * 10}px`;
+    divBoxChildEl.style.height = `${30 + i * 10}px`;
+    divBoxChildEl.style.backgroundColor = getRandomHexColor();
+    boxes.push(divBoxChildEl);
+  }
+
+  boxesEl.append(...boxes);
+}
+
+// очищение
+destroyBtnEl.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  inputEl.value = '';
+  boxesEl.innerHTML = '';
+}
